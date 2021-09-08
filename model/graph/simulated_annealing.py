48599e340aad3eac2c3d6f0b05ea17db634b95ec
swyang <swyang@aimedic.kr>
2021-09-08 19:34:40 +0900

    [NEW] graph-based segmentation using optimizer of simulated annealing

--- model/graph/simulated_annealing.py
+++ model/graph/simulated_annealing.py
@@ -1,18 +1,17 @@
-import  os
-import  glob
-import  cv2
 import  numpy    as  np
+from    scipy.ndimage   import  convolve
 
-from    skimage.measure     import  label
+
+# Neumann neighborhoods
+displi = [-1, 1, 0, 0]
+displj = [0, 0, -1, 1]
 
 
-def _getDisplacement(numNgb=4):
-    ''' Return displacements of 'numNgb' neighbors
+def _funcLogGaussianEnergy(x, pos, theta):
+    ''' Return logarithmic Gibb's energy
     '''
-    if numNgb == 8:
-        return np.argwhere(np.ones((3,3))) - [1, 1]
-    elif numNgb == 4:
-        return np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
+    return (np.log(np.sqrt(2*np.pi*theta[1])) 
+            + .5*((x[pos] - theta[0])**2)/theta[1])
 
 
 def _sgnUnion(c1, c2):
@@ -21,135 +20,157 @@ def _sgnUnion(c1, c2):
     return -1 if c1 == c2 else 1
 
 
-def _coolingSchedular(step, temp, initTemp, mode='gaussian'):
+def _cooling(step, temp, initTemp, mode="gaussian"):
     ''' Return current temperature
     '''
-    if mode == 'exp':
+    if mode == "exp":
         return 0
-    elif mode == 'gaussian':
+    elif mode == "gaussian":
         return initTemp / (1 + np.log(1+step))
     else:
         return -1
 
 
-def _funcLogGaussianEnergy(x, pos, mu, var):
-    ''' Return logarithmic Gibb's energy
+class SimulatedAnnealing(object):
+    ''' Grow-cut segmentation.
+
+    Parameters
+    ----------------
+    image: (H, W) ndarray
+        Input image.
+    seed: (H, W) ndarray
+        Input seed.
+
+    Returns
+    -------
+    region: (H, W) ndarray
+        Segmentation region.
     '''
-    return np.log(np.sqrt(2*np.pi*var)) + .5*((x[pos] - mu)**2)/var
+    def __init__(self, maxIter=100, initTemp=5.e4):
+        # Paremters
+        self.maxIter = maxIter
+        self.initTemp = initTemp
 
+        # Generate numpy random seed
+        np.random.seed(931016)
 
-def _getClassParams(img, lbl):
-    ''' Return class distribution
-    '''
-    classes = np.delete(np.unique(lbl), 0)
-    theta = {cls: [np.mean(img[lbl == cls]),
-                  np.var(img[lbl == cls])] for cls in classes}
+    def getInitPrior(self, image, seed, classes):
+        ''' Return initial prior of seed labels
+        '''
+        # Evaluate prior
+        thetas = [(np.mean(image[seed == cls]), 
+                   np.var(image[seed == cls])) for cls in classes]
 
-    for key, value in theta.items():
-        print(key, value)
+        return thetas
+   
+    def getInitEnergy(self, image, seed, thetas, beta=100):
+        ''' Return Gibb's energy from initial distribution
+        '''
+        # Neumann neighborhoods
+        displi = [-1, 1, 0, 0]
+        displj = [0, 0, -1, 1]
 
-    return theta
-
-
-def _getInitEnergy(img, lbl, theta, beta=100):
-    ''' Return initial Gibb's energy
-    '''
-    energy = 0
-    pos = np.argwhere(lbl > 0)
-    pad = np.pad(lbl, pad_width=1, mode='edge')
-    for i, j in pos:
-        # Read class params
-        cls = lbl[i,j]
-        mu, var = theta[cls][0], theta[cls][1]
-
-        # Singleton energy
-        energy += _funcLogGaussianEnergy(img, (i,j), mu, var)
-
-        # Doubleton energy
-        for dx, dy in _getDisplacement():
-            xi, yj = i+dx, j+dy
-            energy += beta*_sgnUnion(cls, pad[xi+1, yj+1])
-
-    return energy
-
-
-def _getDeltaEnergy(img, lbl, pos, ngbCls, theta, beta=100):
-    ''' Return purturbation of energy by randon walk
-    '''
-    # Get parameters of each classes
-    cls = lbl[pos]
-
-    muPrev, varPrev = theta[cls][0], theta[cls][1]
-    muNext, varNext = theta[ngbCls][0], theta[ngbCls][1]
-
-    # Evaluate singleton energies
-    prevEnergy  = _funcLogGaussianEnergy(img, pos, muPrev, varPrev)
-    nextEnergy  = _funcLogGaussianEnergy(img, pos, muNext, varNext)
-    deltaEnergy = nextEnergy - prevEnergy
-
-    # Evaluate doubleton energies
-    # Larger 'beta' yields more homogeneous classification
-    pad = np.pad(lbl, pad_width=1, mode='edge')
-    for dx, dy in _getDisplacement():
-        xi, yj = pos[0]+dx, pos[1]+dy
-
-        deltaEnergy += beta*(_sgnUnion(ngbCls, pad[xi+1, yj+1]) -
-                             _sgnUnion(cls, pad[xi+1, yj+1]))
-
-    return deltaEnergy
-
-
-def _Idonthowtonamethisfunction(lbl): 
-    ''' Return positions to be updated
-    '''
-    coord = np.argwhere(np.ones_like(lbl))
-    pad = np.pad(lbl, pad_width=1, mode='edge')
-    trial = [[i,j] for i, j in coord 
-             if (0 < pad[i+1,j+1] < 7 and np.sum(pad[i:i+3, j:j+3])/pad[i+1,j+1] != 9) or
-                (pad[i+1,j+1] == 7 and 0 < np.min(pad[i:i+3, j:j+3]) < 7) ]
-
-    return trial 
-
-
-def simulatedAnnealing(img, lbl,
-                       maxIter=10, initTemp=50000, eps=1.e-4):
-    # Generate numpy random seed
-    np.random.seed(931016)
-
-    # Get label information
-    theta = _getClassParams(img, lbl)
-
-    # Evaluate initial potentials
-    energy = _getInitEnergy(img, lbl, theta)
-
-    # Optimization
-    step = 0
-    web  = lbl.copy()
-    classes = np.delete(np.unique(lbl), 0)
-    while step < maxIter:
-        print(step+1, maxIter)
+        # Padding input seed
+        pad = np.pad(seed, pad_width=1, mode="edge")
         
-        # Get temperature
-        temp  = _coolingSchedular(step, 0, initTemp)
-        
-        for i, j in _Idontknowhowtonamethisfunction(web):
-            # Randomly choice a random class except for previous one
-            sample = np.setdiff1d(classes, web[i,j])
-            ngbCls = np.random.choice(sample, 1)[0]
+        # Calculate Gibb's energy 
+        energy = 0
+        for i, j in np.argwhere(seed > 0):
+            # Get current seed class
+            cls = seed[i, j]
 
-            # Evaluate perturbation
-            deltaEnergy = _getDeltaEnergy(img, web, (i,j), ngbCls, theta)
+            # Singleton energy
+            energy += _funcLogGaussianEnergy(image, (i, j), thetas[cls-1])
 
-            # Metropolice criteria
-            zeta = np.random.rand()
-            if (deltaEnergy <= 0 or
-                (deltaEnergy > 0 and np.exp(-deltaEnergy / temp) < zeta)):
-                web[i, j]  = ngbCls
-                energy    += deltaEnergy
+            # Doubleton energy
+            for di, dj in zip(displi, displj):
+                ii, jj = i + di, j + dj
+                
+                # Append energy
+                energy += beta*_sgnUnion(cls, pad[ii+1, jj+1])
 
-        # Update iterations
-        temp  = _coolingSchedular(step, temp, initTemp)
-        step += 1
+        return energy
 
-    return web
+    def getActiveNode(self, region, eps=1.e-13):
+        ''' Return positions to be updated
+        '''
+        # Get number of neighbors
+        weight = convolve(region, np.ones((3, 3)), mode="constant")
 
+        # Set filtering conditions
+        logic0 = np.uint8(np.logical_and(region > 0, weight != 9))
+        logic1 = np.uint8(np.logical_and(region == 0, weight != 0))
+
+        # Get active nodes
+        nodes = np.argwhere(np.logical_or(logic0, logic1))
+
+        return nodes
+
+    def getDeltaEnergy(self, image, region, pos, neighbor, thetas, beta=100):
+        ''' Return purturbation of energy by randon walk
+        '''
+        # Get current class
+        cls = region[pos]
+
+        # Evaluate singleton energies
+        prevEnergy = _funcLogGaussianEnergy(image, pos, thetas[cls-1])
+        nextEnergy = _funcLogGaussianEnergy(image, pos, thetas[neighbor-1])
+        deltaEnergy = nextEnergy - prevEnergy
+
+        # Evaluate doubleton energies
+        # Larger 'beta' yields more homogeneous classification
+        pad = np.pad(region, pad_width=1, mode='edge')
+        for di, dj in zip(displi, displj):
+            ii, jj = pos[0] + di, pos[1] + dj
+
+            deltaEnergy += beta*(_sgnUnion(neighbor, pad[ii+1, jj+1]) -
+                                 _sgnUnion(cls, pad[ii+1, jj+1]))
+
+        return deltaEnergy
+
+    def run(self, image, seed):
+        # Feed region from input seed
+        region = seed.copy()
+
+        # Get list of labeling class
+        classes = np.unique(seed)[1:]    # List out background
+
+        # Get initial priors
+        thetas = self.getInitPrior(image, seed, classes)
+
+        # Evaluate initial potentials
+        energy = self.getInitEnergy(image, seed, thetas)
+
+        # Optimization
+        step, temp = 0, self.initTemp
+        while step < self.maxIter:
+            # Get positions to be updated
+            nodes = self.getActiveNode(region)
+
+            # Perturbe static distribution
+            for i, j in nodes:               
+                # Randomly choice a random class except for previous one
+                samples = np.setdiff1d(classes, region[i, j])
+                neighbor = np.random.choice(samples, 1)[0]
+
+                # Get energy change
+                deltaEnergy = self.getDeltaEnergy(image, region, (i, j), neighbor, thetas)
+
+                # Metropolice criteria
+                zeta = np.random.rand()
+                if (deltaEnergy <= 0 or
+                    (deltaEnergy > 0 and 
+                     np.exp(-deltaEnergy / temp) < zeta)):
+                    # Update region
+                    region[i, j] = neighbor
+
+                    # Update total energy
+                    energy += deltaEnergy
+
+            # Cool down temperature
+            temp = _cooling(step, temp, self.initTemp)
+            
+            # Update step
+            step += 1
+
+        return region
